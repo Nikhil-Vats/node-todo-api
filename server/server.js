e641f33dd418e27b51326232d0afb30276f4d9a1
NikhilPhalange <nikvats5499@gmail.com>
2018-07-14 00:52:45 +0530

    Added Post/todos routes and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,20 +1,34 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
+//express lets us make a server and deploy our application on a local host
+//body-parser lets us send string type JSON objects to server
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/users');
 
-mongoose.promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var app = express();
 
-var Todo = mongoose.model('Todo',{
-    text: {
-        type:String,
-    },
-    completed: {
-        type:Boolean,
-    },
-    completedAt: {
-        type:Number
-    }
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res) => {
+    console.log(req.body);
+    var todo = new Todo({
+        text:req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
-//
+
+//to read data - GET '/todos'
+
+app.listen(3000, () => {
+   console.log('Started on port 3000'); 
+});
+
+//we can also check http statuses like 200,404 for errors go to httpstatuses.com for more info
 //var newTodo = new Todo({
 //    text:'Cook dinner'
 //});
@@ -25,14 +39,24 @@ var Todo = mongoose.model('Todo',{
 //    console.log('Unable to save todo')
 //});
 
-var Todo2 = new Todo({
-    text:'Do exercise',
-    completed:true,
-    completedAt: new Date().getHours()
-});
+//var Todo2 = new Todo({
+//    text:'Something to do'   //typecasting exists so we can set text to a number or a boolean and it will be converted to a string
+//});
 
-Todo2.save().then((doc)=>{
-    console.log(JSON.stringify(doc,undefined,2));
-},(e) => {
-         console.log('Unable to connect')         
-});
+
+//
+//var user = new User({
+//   email:'nikvats5499@gmail.com' 
+//});
+//
+//user.save().then(() => {
+//    console.log('User saved');
+//}, (e) => {
+//   console.log('Unable to save user', e); 
+//});
+
+//Todo2.save().then((doc)=>{
+//    console.log(JSON.stringify(doc,undefined,2));
+//},(e) => {
+//         console.log('Unable to connect');        
+//});
