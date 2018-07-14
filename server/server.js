9bc2ac7118bda5baff02c7304521d2f9777313ef
NikhilPhalange <nikvats5499@gmail.com>
2018-07-14 15:08:05 +0530

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 //express lets us make a server and deploy our application on a local host
 //body-parser lets us send string type JSON objects to server
 var {mongoose} = require('./db/mongoose.js');
@@ -30,6 +31,23 @@ app.get('/todos',(req,res) => {
    }, (e) => {
        res.status(400).send(e);
    });
+});
+
+
+app.get('/todos/:id',(req,res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        
+        res.send({todo});
+    }).catch((e) => {
+       res.status(400).send(); 
+    });
 });
 
 app.listen(3000, () => {
