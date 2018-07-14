efdba24507f81b688c83248eefadc46d7ea40d4f
NikhilPhalange <nikvats5499@gmail.com>
2018-07-15 01:46:20 +0530

    Added authentication for email and pass in users

--- server/models/users.js
+++ server/models/users.js
@@ -1,12 +1,35 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User',{
     email: {
         type:String,
         required:true,
         minLength:1,
-        trim:true
-    }
+        trim:true,
+        unique:true, //makes a unique email
+        validate: {
+            validator: validator.isEmail,
+            //return true for valid and false for invalid    
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password:{
+        type:String,
+        require:true,
+        minLength:6
+    },
+    tokens: [{
+        access:{
+            type:String,
+            require:true
+        },
+        tokens: {
+            
+            type:String,
+            require:true
+        }
+    }]
 });
 
 module.exports = {User};
